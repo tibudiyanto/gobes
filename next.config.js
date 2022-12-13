df9f26c70349ddeb47ea7815de2ceb56d9e73596
Theo Budiyanto <theodorusib@gmail.com>
2022-12-13 18:08:26 +0700

    hardcode domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV == "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    URL: isProd ? "https://sbybus.vercel.app" : "http://localhost:3000",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- utils/requests.ts
+++ utils/requests.ts
@@ -1,8 +1,7 @@
 import ky from "ky-universal";
 
-const { BASE_URL, VERCEL_URL } = process.env;
+const { BASE_URL, URL } = process.env;
 const originAPI = ky.create({ prefixUrl: BASE_URL });
-const url = VERCEL_URL ? `https://${VERCEL_URL}` : "http://localhost:3000";
-const api = ky.create({ prefixUrl: `${url}/api/` });
+const api = ky.create({ prefixUrl: `${process.env.URL}/api/` });
 
 export { originAPI, api };
