5e878bb470e0189df3981df0ff7cfe820945001d
Theo Budiyanto <theodorusib@gmail.com>
2022-12-13 17:15:21 +0700

    yok bs yok

--- utils/requests.ts
+++ utils/requests.ts
@@ -2,6 +2,7 @@ import ky from "ky-universal";
 
 const { BASE_URL, VERCEL_URL } = process.env;
 const originAPI = ky.create({ prefixUrl: BASE_URL });
-const api = ky.create({ prefixUrl: `${VERCEL_URL || "http://localhost:3000"}/api/` });
+const url = VERCEL_URL ? `https://${VERCEL_URL}` : "http://localhost:3000";
+const api = ky.create({ prefixUrl: `${url}/api/` });
 
 export { originAPI, api };
